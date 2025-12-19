7605fc798f44178a8c4a6cb47a680d0491e821a3
Jacob Oakley <jacoboakley21@gmail.com>
2025-12-19 07:25:19 -0600

    Fix some text

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,12 +2,13 @@
 export default new Map([
 ["src/content/services/land-clearing/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fland-clearing%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/services/mulching/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fmulching%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/about/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fabout%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/services/pond-building/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fservices%2Fpond-building%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/portfolio/build-site/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Fbuild-site%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/portfolio/building-pad/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Fbuilding-pad%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/portfolio/large-clearing/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Flarge-clearing%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/portfolio/mulched-land/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Fmulched-land%2Findex.mdoc&astroContentModuleFlag=true")],
+["src/content/portfolio/large-clearing/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Flarge-clearing%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/portfolio/recreational-pond/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Frecreational-pond%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/portfolio/ranch-pond/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Franch-pond%2Findex.mdoc&astroContentModuleFlag=true")]]);
+["src/content/about/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fabout%2Findex.mdoc&astroContentModuleFlag=true")],
+["src/content/portfolio/ranch-pond/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fportfolio%2Franch-pond%2Findex.mdoc&astroContentModuleFlag=true")],
+["src/content/about/text.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fabout%2Ftext.mdoc&astroContentModuleFlag=true")]]);
 		
